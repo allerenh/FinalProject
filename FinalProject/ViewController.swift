d61f5cc44d132bdcabf87cb2b1bf320a41a8725c
allerenh <allerenh@emeal.nttdata.comgit config --global user.email allerenh@emeal.nttdata.com>
2022-11-23 16:18:15 -0500

    first commit

--- FinalProject/ViewController.swift
+++ FinalProject/ViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.label.text = "holaaaa"
     }
 
 
