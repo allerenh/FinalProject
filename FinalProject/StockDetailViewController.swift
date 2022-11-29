7ca938f5ec548dc46868b22e10fe18b3cfc76cde
allerenh <allerenh@emeal.nttdata.comgit config --global user.email allerenh@emeal.nttdata.com>
2022-11-29 13:56:31 -0500

    second commit

---
+++ FinalProject/StockDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StockDetailViewController.swift
+//  FinalProject
+//
+//  Created by Ana Isabel Llerena Huayta on 29/11/22.
+//
+
+import UIKit
+
+class StockDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FinalProject/TransactionSummaryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TransactionSummaryViewController.swift
+//  FinalProject
+//
+//  Created by Ana Isabel Llerena Huayta on 29/11/22.
+//
+
+import UIKit
+
+class TransactionSummaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
